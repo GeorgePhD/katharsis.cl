3e1ab950a6ea7b6291e52c692454bee59bc94ac8
George <lets.devel0pme@gmail.com>
2023-12-09 13:50:30 -0300

    images path modified

--- src/Components/InicioImagesContainer/InicioImagesContainer.jsx
+++ src/Components/InicioImagesContainer/InicioImagesContainer.jsx
@@ -1,18 +1,18 @@
 import './inicio-images-container.css';
-import '../../../public/img/Katharsis.png';
-import '../../../public/img/Colorful-Cool-Lonely-Man-Watching-the-Sunset-Photo-Album-Cover-(1).png';
-import '../../../public/img/Colorful-Cool-Lonely-Man-Watching-the-Sunset-Photo-Album-Cover-(2).png';
-import '../../../public/img/Colorful-Cool-Lonely-Man-Watching-the-Sunset-Photo-Album-Cover.png';
+import katharsisImage1 from '../../../public/img/Katharsis.png';
+import katharsisImage2 from '../../../public/img/Colorful-Cool-Lonely-Man-Watching-the-Sunset-Photo-Album-Cover-(1).png';
+import katharsisImage3 from '../../../public/img/Colorful-Cool-Lonely-Man-Watching-the-Sunset-Photo-Album-Cover-(2).png';
+import katharsisImage4 from '../../../public/img/Colorful-Cool-Lonely-Man-Watching-the-Sunset-Photo-Album-Cover.png';
 const InicioImagesContainer = () => {
 
 
     return (
 
         <div className="inicio-images-container">
-                <img className='katharsis-img' src="/katharsis.cl/public/img/Katharsis.png" alt="imagen de productora katharsis" />
-                <img className='katharsis-img' src="/katharsis.cl/public/img/Colorful-Cool-Lonely-Man-Watching-the-Sunset-Photo-Album-Cover-(1).png" alt="imagen de productora katharsis" />
-                <img className='katharsis-img' src="/katharsis.cl/public/img/Colorful-Cool-Lonely-Man-Watching-the-Sunset-Photo-Album-Cover-(2).png" alt="imagen de productora katharsis" />
-                <img className='katharsis-img' src="/katharsis.cl/public/img/Colorful-Cool-Lonely-Man-Watching-the-Sunset-Photo-Album-Cover.png" alt="imagen de productora katharsis" />
+                <img className='katharsis-img' src={katharsisImage1} alt="imagen de productora katharsis" />
+                <img className='katharsis-img' src={katharsisImage2} alt="imagen de productora katharsis" />
+                <img className='katharsis-img' src={katharsisImage3} alt="imagen de productora katharsis" />
+                <img className='katharsis-img' src={katharsisImage4} alt="imagen de productora katharsis" />
             </div>
     )
 }
